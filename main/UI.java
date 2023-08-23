159e46cce8c1b94aa449db150b798fd4f5fbedc2
Luca Nicotra <Lsnicotra44@gmail.com>
2023-08-23 09:30:56 -0500

    Inventory groundwork

--- main/UI.java
+++ main/UI.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
@@ -13,6 +14,9 @@ public class UI {
      public String message = "";
      int messageCounter = 0;
      public boolean gameFinished = false;
+     public int slotCol = 0;
+     public int slotRow = 0;
+
 
      public UI(GamePanel gp){
           this.gp = gp;
@@ -35,7 +39,8 @@ public class UI {
 
           }
           if(gp.gameState == gp.pauseState){
-               drawPauseScreen();
+               //drawPauseScreen();
+               drawInventory();
           }
      }
 
@@ -52,6 +57,48 @@ public class UI {
           int length = (int)g2.getFontMetrics().getStringBounds(text, g2).getWidth();
           int x = gp.screenWidth/2 - length/2;
           return x;
+     }
+
+     public void drawInventory(){
+
+          //Window
+          int frameX = gp.tileSize*9;
+          int frameY = gp.tileSize;
+          int frameWidth = gp.tileSize*5;
+          int frameHeight = gp.tileSize*5;
+          drawSubWindow(frameX, frameY, frameWidth,frameHeight);
+
+          //Slot
+          final int slotXStart = frameX + 20;
+          final int slotYStart = frameY + 20;
+          int slotX = slotXStart;
+          int slotY = slotYStart;
+
+          //Cursor
+          int cursorX = slotXStart + (gp.tileSize * slotCol);
+          int cursorY = slotYStart + (gp.tileSize * slotRow);
+          int cursorWidth = gp.tileSize;
+          int cursorHeight = gp.tileSize;
+
+          //Draw Cursor
+          g2.setColor(Color.white);
+          g2.setStroke(new BasicStroke(3));
+          g2.drawRoundRect(cursorX, cursorY, cursorWidth, cursorHeight, 10, 10);
+
+     }
+
+     public void drawSubWindow(int x, int y, int width, int height){
+
+          //Black Opaque Rectangle
+          Color c = new Color(0,0,0, 180); //Black
+          g2.setColor(c);
+          g2.fillRoundRect(x, y, width, height, 35, 35);
+
+          //Gold Frame
+          c = new Color(204,204,0); //Gold
+          g2.setColor(c);
+          g2.setStroke(new BasicStroke(5));
+          g2.drawRoundRect(x+5, y+5, width-10, height-10, 25, 25);
      }
 
 }
